befbc4e3e6a9717698885b4aa5d4c73a2085a00b
Timothy <tsimmons15@knights.ucf.edu>
2017-09-27 15:02:44 -0400

    InitialCommit
    Setting up for project EdgeFilter.

---
+++ EdgeFilter/EdgeFilter/EdgeFilter.cs
@@ -0,0 +1,72 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+
+namespace EdgeFilter
+{
+    class EdgeFilter
+    {
+
+        private static int cellSize = 0;
+        public static int CellSize
+        {
+            get
+            {
+                return cellSize;
+            }
+
+            set
+            {
+                if (value % 5 == 0)
+                    cellSize = value;
+            }
+        }
+
+        public static int VerticalCells
+        {
+            get
+            {
+                return OriginalImage.Rows / CellSize;
+            }
+        }
+
+        public static int HorizontalCells
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public static byte OutOfBoundMask
+        {
+            get; set;
+        }
+
+        public static Image<Gray, Byte> OriginalImage
+        {
+            get; set;
+        }
+
+        public static Image<Gray, Byte> FilteredImage
+        {
+            get; set;
+        }
+
+        public static void FilterImage(Mat img)
+        {
+            FilterImage(img.ToImage<Gray, Byte>());
+        }
+
+        public static void FilterImage(Image<Gray, Byte> img)
+        {
+            for(int i = 0; i < VerticalCells; i++)
+            {
+                for(int j = 0; j < HorizontalCells; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ EdgeFilter/EdgeFilter/Program.cs
@@ -0,0 +1,25 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdgeFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoCapture capture = new VideoCapture(0);
+
+            Mat capturedImg = capture.QueryFrame();
+
+            capturedImg.Save("C:\\Users\\student-eac\\Desktop\\img.jpg");
+
+            EdgeFilter.FilterImage(capturedImg);
+
+        }
+    }
+}
