0ffc31fbe27b1b7d5c996f3c4fbaa475ce4a05bd
tsimmons15 <tsimmons15@valenciacollege.edu>
2018-06-26 22:09:09 -0400

    Setting up the VS17 project that will contain the code. Also ported some code from C#/Server to SinglePlayer since they should be fairly similar.

---
+++ C#/SinglePlayer/SinglePlayer/Database/DatabaseInteraction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglePlayer.Database
+{
+    class DatabaseInteraction
+    {
+        private void Encrypt(string password)
+        {
+            SHA512Managed test = new SHA512Managed();
+            byte[] hash = test.ComputeHash(Miscellaneous.StringToBytes(password));
+
+        }
+    }
+}
---
+++ C#/SinglePlayer/SinglePlayer/Miscellaneous.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglePlayer
+{
+    class Miscellaneous
+    {
+        public static byte[] StringToBytes(string text)
+        {
+            byte[] buff = new byte[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                buff[i] = (byte)text[i];
+            }
+
+            return buff;
+        }
+    }
+}
